364924927313ac80195034c21cb1f47960261dc3
leslie-choi <leslie_choi@yeah.net>
2020-04-20 10:09:15 -0700

    mac init

--- store/index.js
+++ store/index.js
@@ -3,25 +3,33 @@ import Vuex from 'vuex'
 import geo from './geo'
 import home from './home'
 
-
 Vue.use(Vuex)
-
 
 export const state = () => {
   geo
 }
 
 export const actions = {
-  async nuxtServerInit ({
-    commit
-  }, { req, app }) {
-    const { status, data: { province, city } } = await app.$axios.get('/geo/getPosition')
-    commit('geo/setPosition', status === 200 ? { city, province } : { city: '', province: '' })
+  async nuxtServerInit ({ commit }, { req, app }) {
+    const {
+      status,
+      data: { province, city }
+    } = await app.$axios.get('/geo/getPosition')
+    commit(
+      'geo/setPosition',
+      status === 200 ? { city, province } : { city: '', province: '' }
+    )
 
-    const { status: status2, data: { menu } } = await app.$axios.get('/geo/menu')
+    const {
+      status: status2,
+      data: { menu }
+    } = await app.$axios.get('/geo/menu')
     commit('home/setMenu', status2 === 200 ? menu : [])
 
-    const { status: status3, data: { result } } = await app.$axios.get('/search/hotPlace', {
+    const {
+      status: status3,
+      data: { result }
+    } = await app.$axios.get('/search/hotPlace', {
       params: {
         city: app.store.state.geo.position.city.replace('市', '')
       }
